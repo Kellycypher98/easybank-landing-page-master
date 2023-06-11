d1bf5a4b5c4efc932046d4d8b5fc5029411faa3e
Kellycypher98 <kelvinboateng94@outlook.com>
2023-06-11 18:28:28 +0000

    final changes

---
+++ app.js
@@ -0,0 +1,9 @@
+function toggleMobileMenu() {
+  const mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.classList.toggle("hidden");
+}
+
+function closeMobileMenu() {
+  const mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.classList.add("hidden");
+}
